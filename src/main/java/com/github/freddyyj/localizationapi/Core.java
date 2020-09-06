b0ebc3bae7e7a6838aa09e61556724c80d621680
김영재 <kyj141100@naver.com>
2020-09-06 13:41:38 +0900

    Add language set command

--- src/main/java/com/github/freddyyj/localizationapi/Core.java
+++ src/main/java/com/github/freddyyj/localizationapi/Core.java
@@ -1,6 +1,9 @@
 package com.github.freddyyj.localizationapi;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -31,6 +34,23 @@ public class Core extends JavaPlugin implements Listener {
         getLogger().info("LocalizationAPI v0.0.2 loaded!");
         getLogger().info("Loaded languages: "+ Arrays.toString(languageCodeList));
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args[0].equals("set") && args.length>=2 && sender instanceof Player){ // /lang set <language code> [player]
+            if (args.length==2){
+                try {
+                    PlayerLocalization.fromPlayer((Player) sender).setLanguage(Language.getLanguage(args[1]));
+                } catch (IOException e) {
+                    sender.sendMessage("No language code found: "+args[1]);
+                    return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
         PlayerLocalization player=PlayerLocalization.fromPlayer(e.getPlayer());
