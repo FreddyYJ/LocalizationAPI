83854b5c61edeb0d7e68ea13f2184ac45e0facbc
김영재 <kyj141100@naver.com>
2020-09-10 12:35:19 +0900

    Improve command interface

--- src/main/java/com/github/freddyyj/localizationapi/Core.java
+++ src/main/java/com/github/freddyyj/localizationapi/Core.java
@@ -2,6 +2,7 @@ package com.github.freddyyj.localizationapi;
 
 import com.github.freddyyj.localizationapi.langfile.PlayerLanguageData;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Core extends JavaPlugin implements Listener {
+    public static final String PREFIX= ChatColor.AQUA+"["+ChatColor.GREEN+"LocalizationAPI"+ChatColor.AQUA+"] "+ChatColor.WHITE;
     public static File dataFolder;
     private String[] languageCodeList;
     private static PlayerLanguageData languageSavefile;
@@ -49,8 +51,17 @@ public class Core extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args[0].equals("get") && sender instanceof Player){ // /lang get [player]
-            sender.sendMessage("Current language: "+PlayerLocalization.fromPlayer((Player) sender).getLanguage().getLanguageCode());
+        if (args.length==0){
+            sender.sendMessage(PREFIX+"Command list:");
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang"+ChatColor.WHITE+": Show command list.");
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get [player]"+ChatColor.WHITE+": Get [player] or your current language.");
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code> [player]"+ChatColor.WHITE+": Change [player] or your current language.");
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang test"+ChatColor.WHITE+": Test with default message(message.welcome).");
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang list"+ChatColor.WHITE+": Show language list.");
+            return true;
+        }
+        else if (args[0].equals("get") && sender instanceof Player){ // /lang get [player]
+            sender.sendMessage(PREFIX+"Current language: "+PlayerLocalization.fromPlayer((Player) sender).getLanguage().getLanguageCode());
             return true;
         }
         else if (args[0].equals("set") && args.length>=2 && sender instanceof Player){ // /lang set <language code> [player]
@@ -58,22 +69,33 @@ public class Core extends JavaPlugin implements Listener {
                 try {
                     PlayerLocalization.fromPlayer((Player) sender).setLanguage(Language.getLanguage(args[1]));
                     languageSavefile.setPlayerLanguageCode(((Player) sender).getUniqueId(),args[1]);
+
+                    sender.sendMessage(PREFIX+"Language set to "+args[1]);
                 } catch (IOException e) {
-                    sender.sendMessage("No language code found: "+args[1]);
+                    sender.sendMessage(PREFIX+"No language code found: "+args[1]);
                     return false;
                 }
                 return true;
             }
+            return true;
         }
         else if (args[0].equals("test") && sender instanceof Player){ // /lang test
-            PlayerLocalization.fromPlayer((Player) sender).sendMessage("message.welcome");
+            sender.sendMessage(PREFIX+PlayerLocalization.fromPlayer((Player) sender).getMessage("message.welcome"));
             return true;
         }
         else if (args[0].equals("list")){ // /lang list
-            sender.sendMessage("Current language list: "+Arrays.toString(languageCodeList));
+            sender.sendMessage(PREFIX+"Current language list: "+Arrays.toString(languageCodeList));
             return true;
         }
-        return false;
+        else {
+            sender.sendMessage(PREFIX + "Unknown Command! Command list:");
+            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang" + ChatColor.WHITE + ": Show command list.");
+            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang get [player]" + ChatColor.WHITE + ": Get [player] or your current language.");
+            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang set <language code> [player]" + ChatColor.WHITE + ": Change [player] or your current language.");
+            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang test" + ChatColor.WHITE + ": Test with default message(message.welcome).");
+            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang list" + ChatColor.WHITE + ": Show language list.");
+            return true;
+        }
     }
     public static PlayerLanguageData getLanguageData(){
         return languageSavefile;
@@ -88,7 +110,6 @@ public class Core extends JavaPlugin implements Listener {
         player.sendMessage("message.welcome2");
 
         getLogger().info(player.toPlayer().getName()+" entered!");
-
     }
     @EventHandler(ignoreCancelled = true)
     public void onPlayerQuit(PlayerQuitEvent e){
