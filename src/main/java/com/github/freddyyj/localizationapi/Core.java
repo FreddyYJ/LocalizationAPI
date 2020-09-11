8ae5e2345f6fe7b534fb4259312b2f7660440bda
김영재 <kyj141100@naver.com>
2020-09-11 12:01:12 +0900

    Add command to get or set other player language

--- src/main/java/com/github/freddyyj/localizationapi/Core.java
+++ src/main/java/com/github/freddyyj/localizationapi/Core.java
@@ -52,26 +52,36 @@ public class Core extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length==0){
-            sender.sendMessage(PREFIX+"Command list:");
-            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang"+ChatColor.WHITE+": Show command list.");
-            if (sender.hasPermission("localization.get.other"))
-                sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get [player]"+ChatColor.WHITE+": Get [player] or your current language.");
-            else if (sender.hasPermission("localization.get"))
-                sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get"+ChatColor.WHITE+": Get your current language.");
-            if (sender.hasPermission("localization.set.other"))
-                sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code> [player]"+ChatColor.WHITE+": Change [player] or your current language.");
-            else if (sender.hasPermission("localization.set"))
-                sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code>"+ChatColor.WHITE+": Change your current language.");
-            if (sender.hasPermission("localization.test")) sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang test"+ChatColor.WHITE+": Test with default message(message.welcome).");
-            if (sender.hasPermission("localization.list")) sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang list"+ChatColor.WHITE+": Show language list.");
+            printCommandList(sender);
             return true;
         }
         else if (args[0].equals("get") && sender instanceof Player && sender.hasPermission("localization.get")){ // /lang get [player]
-            sender.sendMessage(PREFIX+"Current language: "+PlayerLocalization.fromPlayer((Player) sender).getLanguage().getLanguageCode());
+            if (sender.hasPermission("localization.get.other") && args.length==2){
+                Player targetPlayer=Bukkit.getPlayer(args[1]);
+                if (targetPlayer==null) throw new NullPointerException("Not exist player: "+args[1]);
+                else sender.sendMessage(PREFIX+"Current language of "+targetPlayer.getName()+": "+PlayerLocalization.fromPlayer(targetPlayer).getLanguage().getLanguageCode());
+            }
+            else if (args.length==1) sender.sendMessage(PREFIX+"Current language: "+PlayerLocalization.fromPlayer((Player) sender).getLanguage().getLanguageCode());
+            else printCommandError(sender);
             return true;
         }
         else if (args[0].equals("set") && args.length>=2 && sender instanceof Player && sender.hasPermission("localization.set")){ // /lang set <language code> [player]
-            if (args.length==2){
+            if (args.length==3 && sender.hasPermission("localization.set.other")){
+                Player targetPlayer=Bukkit.getPlayer(args[2]);
+                if (targetPlayer==null) throw new NullPointerException("Not exist player: "+args[2]);
+                else{
+                    try {
+                        PlayerLocalization.fromPlayer(targetPlayer).setLanguage(Language.getLanguage(args[1]));
+                        languageSavefile.setPlayerLanguageCode((targetPlayer).getUniqueId(),args[1]);
+
+                        sender.sendMessage(PREFIX+"Language of "+targetPlayer.getName()+" set to "+args[1]);
+                    } catch (IOException e) {
+                        sender.sendMessage(PREFIX+"No language code found: "+args[1]);
+                        return true;
+                    }
+                }
+            }
+            else if (args.length==2){
                 try {
                     PlayerLocalization.fromPlayer((Player) sender).setLanguage(Language.getLanguage(args[1]));
                     languageSavefile.setPlayerLanguageCode(((Player) sender).getUniqueId(),args[1]);
@@ -79,10 +89,11 @@ public class Core extends JavaPlugin implements Listener {
                     sender.sendMessage(PREFIX+"Language set to "+args[1]);
                 } catch (IOException e) {
                     sender.sendMessage(PREFIX+"No language code found: "+args[1]);
-                    return false;
+                    return true;
                 }
                 return true;
             }
+            else printCommandError(sender);
             return true;
         }
         else if (args[0].equals("test") && sender instanceof Player && sender.hasPermission("localization.test")){ // /lang test
@@ -94,17 +105,40 @@ public class Core extends JavaPlugin implements Listener {
             return true;
         }
         else {
-            sender.sendMessage(PREFIX + "Unknown Command! Command list:");
-            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang" + ChatColor.WHITE + ": Show command list.");
-            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang get [player]" + ChatColor.WHITE + ": Get [player] or your current language.");
-            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang set <language code> [player]" + ChatColor.WHITE + ": Change [player] or your current language.");
-            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang test" + ChatColor.WHITE + ": Test with default message(message.welcome).");
-            sender.sendMessage(PREFIX + ChatColor.YELLOW + "/lang list" + ChatColor.WHITE + ": Show language list.");
+            printCommandError(sender);
             return true;
         }
     }
     public static PlayerLanguageData getLanguageData(){
         return languageSavefile;
+    }
+    private void printCommandError(CommandSender sender){
+        sender.sendMessage(PREFIX + "Unknown Command! Command list:");
+        sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang"+ChatColor.WHITE+": Show command list.");
+        if (sender.hasPermission("localization.get.other"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get [player]"+ChatColor.WHITE+": Get [player] or your current language.");
+        else if (sender.hasPermission("localization.get"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get"+ChatColor.WHITE+": Get your current language.");
+        if (sender.hasPermission("localization.set.other"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code> [player]"+ChatColor.WHITE+": Change [player] or your current language.");
+        else if (sender.hasPermission("localization.set"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code>"+ChatColor.WHITE+": Change your current language.");
+        if (sender.hasPermission("localization.test")) sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang test"+ChatColor.WHITE+": Test with default message(message.welcome).");
+        if (sender.hasPermission("localization.list")) sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang list"+ChatColor.WHITE+": Show language list.");
+    }
+    private void printCommandList(CommandSender sender){
+        sender.sendMessage(PREFIX+"Command list:");
+        sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang"+ChatColor.WHITE+": Show command list.");
+        if (sender.hasPermission("localization.get.other"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get [player]"+ChatColor.WHITE+": Get [player] or your current language.");
+        else if (sender.hasPermission("localization.get"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang get"+ChatColor.WHITE+": Get your current language.");
+        if (sender.hasPermission("localization.set.other"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code> [player]"+ChatColor.WHITE+": Change [player] or your current language.");
+        else if (sender.hasPermission("localization.set"))
+            sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang set <language code>"+ChatColor.WHITE+": Change your current language.");
+        if (sender.hasPermission("localization.test")) sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang test"+ChatColor.WHITE+": Test with default message(message.welcome).");
+        if (sender.hasPermission("localization.list")) sender.sendMessage(PREFIX+ChatColor.YELLOW+"/lang list"+ChatColor.WHITE+": Show language list.");
     }
 
     @EventHandler
